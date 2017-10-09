3d810791e0957896ac921c02fbe80f37fbdea72a
Virginijus Digrys <virginijus.digrys@autentek.de>
2017-10-09 19:21:49 +0300

    Initial code:   ionic tab view   mapbox-gl displays and focuses on device location

---
+++ Mapbox-Gl-maps/src/pages/home/home.ts
@@ -0,0 +1,72 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import mapboxgl from 'mapbox-gl';
+import { Geolocation, Geoposition } from '@ionic-native/geolocation';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  Coordinates: any;
+  watch: any;
+
+  constructor(public navCtrl: NavController, private geolocation: Geolocation) {
+  }
+
+  ionViewDidEnter() {
+
+    /*Initializing geolocation*/
+    let options = {
+      frequency: 3000,
+      enableHighAccuracy: true
+    };
+    let watch = this.geolocation.watchPosition(options);
+    watch.subscribe((position: Geoposition) => {
+      // data can be a set of coordinates, or an error (if an error occurred).
+      // data.coords.latitude
+      // data.coords.longitude
+      console.log(position);
+      this.Coordinates = position.coords;
+      this.executemap();
+    });
+  }
+
+  // for future use one time get position asynchroniously
+  getUserPosition() {
+    let options = {
+      enableHighAccuracy: true
+    };
+
+    this.geolocation.getCurrentPosition(options).then((pos: Geoposition) => {
+
+      this.Coordinates = pos.coords;
+      console.log(pos);
+
+    }, (err: PositionError) => {
+      console.log("error : " + err.message);
+    });
+  }
+
+  executemap() {
+    /*Initializing Map*/
+    mapboxgl.accessToken = 'pk.eyJ1IjoiaWd1cmphbm93IiwiYSI6ImNpdmIyNnk1eTAwNzgyenBwajhnc2tub3cifQ.dhXaJJHqLj0_thsU2qTxww';
+    var map = new mapboxgl.Map({
+      style: 'mapbox://styles/mapbox/streets-v9',
+      center: [this.Coordinates.longitude, this.Coordinates.latitude], //[-74.0066, 40.7135],
+      zoom: 16,
+      pitch: 80,
+      minZoom: 7.5, //restrict map zoom - buildings not visible beyond 13
+      maxZoom: 17,
+      container: 'map'
+    });
+
+    this.geolocation.getCurrentPosition().then((resp) => {
+      // resp.coords.latitude
+      // resp.coords.longitude
+      this.Coordinates = resp.coords;
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
+  }
+}
