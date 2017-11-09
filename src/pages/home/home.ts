be056d9a2f895d29ff925dc9a6b1897537f3d877
Virginijus Digrys <virginijus.digrys@autentek.de>
2017-11-09 15:14:22 +0200

    trying to assign image url to img tag

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,12 +4,16 @@ import * as L from 'leaflet';
 import 'leaflet.markercluster';
 import 'leaflet-offline';
 import { tilesDb } from './tilesDb';
+import { File } from '@ionic-native/file';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 import { Helper } from '../../classes/Helper';
 import { DBC } from '../../classes/DBC';
 import { DB_Handler } from '../../classes/DB_Handler';
 import { Platform } from 'ionic-angular';
 import { DB_Updater } from '../../classes/DB_Updater';
+
+import { FileTransfer, FileTransferObject } from '@ionic-native/file-transfer'
 
 @Component({
   selector: 'page-home',
@@ -19,30 +23,61 @@ export class HomePage {
   @ViewChild('map') mapContainer: ElementRef;
   map: any;
   center: L.PointTuple;
+  routeImage: SafeUrl;
+  imageObject: any;
+  routeText: string;
 
-  constructor(public navCtrl: NavController, private platform: Platform, private updater: DB_Updater) { }
+  constructor(public navCtrl: NavController, private sanitize: DomSanitizer, private transfer: FileTransfer, private platform: Platform, private updater: DB_Updater) { }
+
+
+  private downloadImage(fileTransfer: FileTransferObject, imgFileName: string, outputName: string) {
+    let fileManager = new File();
+    let imageAddress = Helper.WEBSERVER_URL + encodeURI(imgFileName);
+    fileTransfer.download(imageAddress, fileManager.dataDirectory + outputName)
+    .then(entry => {
+      console.log(`Image download from: ${imageAddress}`)
+      console.log(`Image download completed: ${JSON.stringify(this.sanitize.bypassSecurityTrustUrl(entry.toURL()))}`)
+      this.routeImage = this.sanitize.bypassSecurityTrustUrl(entry.toInternalURL());
+      // fileManager.readAsDataURL(entry.toURL()).then(value => this.routeImage = value, reason => this.failInfo = "Fail: " + reason);
+    }, error => {
+      console.error(`Download error URL: [${imgFileName}]`)
+    }).catch(error => {
+      console.error(`Download error URL: [${imgFileName}]`)
+    })
+  }
+
 
   ionViewDidEnter() {
     console.log("ionViewDidEnter");
-
     this.platform.ready().then(() => {
+      
       console.log('Platform is ready!')
       let dbHandler = DB_Handler.getInstance();
       dbHandler.initialize().then(() => {
         this.updater.execute(["getVersions", DBC.DATABASE_TABLE_STATE, "checkForUpdates"]).then(() => {
           console.log("updater finished!")
           let table = DBC.DB_ROUTE;
-          let sqlQry = `SELECT ${table.fields[3]},${table.fields[13]} FROM ${table.getTableName()} WHERE ${table.fields[2]}=1;`;
+          let sqlQry = `SELECT ${table.fields[3]},${table.fields[6]},${table.fields[13]} FROM ${table.getTableName()} WHERE ${table.fields[2]}=1;`;
           console.log(`SQL QUERY: ${sqlQry}`)
           let dbh = DB_Handler.getInstance();
           let db = dbh.getWritableDatabase();
+          
+          // this.fileManager.dataDirectory + outputName
+          let fileManager = new File();
+
           db.executeSql(sqlQry, []).then(result => {
             let markerGroup = L.markerClusterGroup();
             for (var i = 0; i < result.rows.length; i++) {
               let row = result.rows.item(i);
               let center = JSON.parse(row.center);
               let marker: any = L.marker([center[0], center[1]]).on('click', () => {
-                alert(`Route: ${row.title}`);
+                // let imageUrl = fileManager.dataDirectory + row.image.replace(Helper.REPLACE_ROUTE_IMAGE_PATH, "")
+                // this.routeImage = imageUrl;
+                // alert(`Route: ${row.title} ${imageUrl}`);
+                const fileTransfer: FileTransferObject = this.transfer.create();
+                let outputName = row.image.replace(Helper.REPLACE_ROUTE_IMAGE_PATH, "")
+                this.downloadImage(fileTransfer, row.image, outputName);
+                this.routeText = row.title;
               })
               markerGroup.addLayer(marker);
             }
