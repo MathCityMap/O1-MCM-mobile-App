e2437369612d21251b0f3c77bdf347ace9ac8884
Immanuel Scheerer <immanuel.scheerer@autentek.de>
2017-12-15 17:52:50 +0100

    Fix compilation errors

--- src/services/orm-service.ts
+++ src/services/orm-service.ts
@@ -1,12 +1,9 @@
 import {Injectable} from "@angular/core";
 import {checkAvailability} from "@ionic-native/core";
 import {SQLite} from '@ionic-native/sqlite'
-import {createConnection, Connection } from "typeorm";
-import {CordovaConnectionOptions} from 'typeorm/browser/driver/cordova/CordovaConnectionOptions';
+import {createConnection, Connection} from "typeorm";
 
 import {InitialMigration1513274191111} from '../migration/1513274191111-InitialMigration';
-import {WebSqlConnectionOptions} from 'typeorm/browser/driver/websql/WebSqlConnectionOptions';
-import {BaseConnectionOptions} from 'typeorm/browser/connection/BaseConnectionOptions';
 import {User} from '../entity/User';
 import {State} from '../entity/State';
 import {Task} from '../entity/Task';
@@ -24,32 +21,41 @@ export class OrmService {
       return this.connection;
     }
     const sqliteAvailable = checkAvailability(SQLite.getPluginRef(), null, SQLite.getPluginName()) === true;
-    const sqliteOptions: CordovaConnectionOptions = {
-      type: 'cordova',
-      location: 'default',
-      database: 'mcm_db.sqlite3'
-    };
-    const websqlOptions: WebSqlConnectionOptions = {
-      type: 'websql',
-      version: '1.0',
-      description: 'MCM DB',
-      size: 2 * 1024 * 1024,
-      database: 'mcm'
-    };
-    const options: BaseConnectionOptions = sqliteAvailable ? sqliteOptions : websqlOptions;
-    options.logging = ['error', 'query', 'schema'];
-    options.logger = 'simple-console';
-    options.synchronize = false;
-    options.entities = [
+    const entities = [
       User,
       Route,
       State,
       Task
     ];
-    options.migrations = true;
-    options.migrations = [
+    const migrations = [
       InitialMigration1513274191111
     ];
-    return this.connection = await createConnection(options);
+    if (sqliteAvailable) {
+      return this.connection = await createConnection({
+        type: 'cordova',
+        location: 'default',
+        database: 'mcm_db.sqlite3',
+        logging: ['error', 'query', 'schema'],
+        logger: 'simple-console',
+        synchronize: false,
+        entities: entities,
+        migrationsRun: true,
+        migrations: migrations
+      });
+    } else {
+      return this.connection = await createConnection({
+        type: 'websql',
+        version: '1.0',
+        description: 'MCM DB',
+        size: 2 * 1024 * 1024,
+        database: 'mcm',
+        logging: ['error', 'query', 'schema'],
+        logger: 'simple-console',
+        synchronize: false,
+        entities: entities,
+        migrationsRun: true,
+        migrations: migrations
+      });
+    }
   }
 }
