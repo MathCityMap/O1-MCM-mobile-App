1bfcc8169bbb9ec7e1b9a249801933afa4e28e2a
Hanna Achenbach <hanna.achenbach@autentek.de>
2017-12-20 10:47:16 +0100

    dummy content for info and settings page

--- src/pages/info/info.module.ts
+++ src/pages/info/info.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { InfoPage } from './info';
+import { MCMHeaderModule } from '../../components/mcm-header/mcm-header.module';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { InfoPage } from './info';
   ],
   imports: [
     IonicPageModule.forChild(InfoPage),
+    MCMHeaderModule,
   ],
 })
 export class InfoPageModule {}
