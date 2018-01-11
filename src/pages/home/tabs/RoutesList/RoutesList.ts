2a2b47a0d11928c4cb3485937ba140e920d5623f
Immanuel Scheerer <immanuel.scheerer@autentek.de>
2018-01-11 17:20:28 +0100

    Sort routes 1. by downloaded or not download, 2. distance, 3. title

--- src/pages/home/tabs/RoutesList/RoutesList.ts
+++ src/pages/home/tabs/RoutesList/RoutesList.ts
@@ -14,7 +14,6 @@ import { ModalsService } from '../../../../services/modals-service';
 })
 export class RoutesListPage {
     public items: Route[] = [];
-    public activeDownload: Route = null;
 
     modal: any;
 
@@ -28,14 +27,13 @@ export class RoutesListPage {
         if (this.items.length === 0) {
             this.items = await this.ormService.getPublicRoutes();
         }
-        if (Helper.myLocation) {
-            this.sortItemsByDistance();
-        } else {
+        this.sortItems();
+        if (!Helper.myLocation) {
             try {
                 const position = await this.geolocation.getCurrentPosition();
                 if (position && position.coords) {
                     Helper.myLocation = position;
-                    this.sortItemsByDistance();
+                    this.sortItems();
                 }
             } catch (e) {
                 console.log("could not obtain position");
@@ -44,8 +42,12 @@ export class RoutesListPage {
         }
     }
 
-    sortItemsByDistance() {
+    sortItems() {
         this.items.sort((a, b) => {
+            if (a.downloaded && !b.downloaded)
+                return -1;
+            if (!a.downloaded && b.downloaded)
+                return 1;
             const distA = a.getDistance();
             const distB = b.getDistance();
             if (distA > distB) {
@@ -53,7 +55,7 @@ export class RoutesListPage {
             } else if (distA < distB) {
                 return -1;
             }
-            return 0;
+            return a.title.localeCompare(b.title);
         });
     }
 
