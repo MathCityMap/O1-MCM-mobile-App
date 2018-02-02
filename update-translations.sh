fc1773eefaf833aa6ff7f58e7e8de8ce5556ba2c
Immanuel Scheerer <immanuel.scheerer@autentek.de>
2018-02-02 15:14:43 +0100

    Fix double escaped back slashes

--- update-translations.sh
+++ update-translations.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 [ -z "$1" ] && echo "Usage: $0 <downloaded zip file>" && exit 1
+[ "$(jq --version)" != "jq-1.5" ] && echo "You need to install version 1.5 of 'jq' (https://stedolan.github.io/jq/)." && exit 1
 basedir=$(pwd)
 file=$(basename $1)
 tmpdir=/tmp/$file
@@ -11,7 +12,7 @@ for n in $(find . -name *.xml)
   do
   lang=$(echo $n | sed -e "s/.*\(..\)\/strings.xml/\1/")
   echo $n
-  $basedir/node_modules/.bin/xml2json $n | jq '.resources.string | map({"key": .name | tostring, "value": .["$t"]}) | from_entries' > $basedir/src/assets/localization/$lang.json
+  $basedir/node_modules/.bin/xml2json $n | jq '.resources.string | map({"key": .name | tostring, "value": .["$t"]}) | from_entries' | sed -e "s/\\\\\\\\n/\\\\n/g" | sed -e "s/\\\\\\\\'/'/g"> $basedir/src/assets/localization/$lang.json
 done
 # add missing english keys from german file
 jq -s '.[0] * .[1]' $basedir/src/assets/localization/de.json $basedir/src/assets/localization/en.json > $basedir/src/assets/localization/en.tmp.json
