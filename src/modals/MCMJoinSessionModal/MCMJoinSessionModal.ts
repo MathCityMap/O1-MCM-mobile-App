7611ba47149cbd02dae47b9cf31ce78d11cf54f6
Filip Balonis <filip.balonis@autentek.de>
2018-08-01 18:19:13 +0200

    refs #35 Dynamisches Ausgrauen der Inputfelder

--- src/modals/MCMJoinSessionModal/MCMJoinSessionModal.ts
+++ src/modals/MCMJoinSessionModal/MCMJoinSessionModal.ts
@@ -13,6 +13,7 @@ export class MCMJoinSessionModal {
     teamMemberNames: string;
     teamMemberArray: string[] = [];
     showError: boolean;
+    codeInput: boolean = false;
 
     constructor(private viewCtrl: ViewController) {
 
@@ -27,6 +28,41 @@ export class MCMJoinSessionModal {
     cancel() {
         this.viewCtrl.dismiss();
     }
+
+    start() {
+        console.log(this.teamName);
+        if(this.teamMemberNames != null && this.teamMemberNames != "") {
+            this.teamMemberArray.push(this.teamMemberNames);
+        }
+        console.log(this.teamMemberArray);
+    }
+
+    checkInputField() {
+        if(this.teamName != "" && this.teamName != null){
+            if(this.teamMemberNames != "" && this.teamMemberNames != null) {
+                this.codeInput = true;
+            }
+            else if(this.teamMemberArray.length != 0) {
+                //Funktioniert, wenn sich Array-Einträge dynamisch anpassen, bei Neubennenung 
+                if (this.teamMemberArray.some(function (element) {if (element != "" && element != undefined) {return true;} else {return false;}})) {
+                    this.codeInput = true;
+                }
+                else {
+                    this.codeInput = false;
+                }
+            }
+            else {
+                this.codeInput = false;
+            }
+
+        }
+        else {
+            this.codeInput = false;
+        }
+        return this.codeInput;
+    }
+
+
 
     addTeamMemberName(name: string) {
         this.teamMemberArray.push(name);
