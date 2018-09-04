f89ffcc926041a88c5bba2370f54cac669119966
Immanuel Scheerer <immanuel.scheerer@autentek.de>
2018-09-04 13:49:05 +0200

    need to fix ng-swagger-gen

--- src/app/api/models/session-user.ts
+++ src/app/api/models/session-user.ts
@@ -5,7 +5,7 @@ export class SessionUser {
     last_active_at?: string;
     id?: any;
     team_name?: string;
-    team_members_array?: Array&lt;string&gt;;
+    team_members_array?: string[];
     token?: string;
     deleted?: boolean;
     wp_user_id?: number;
