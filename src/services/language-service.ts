db9de4e07d48e241951962115bd3ab11e5a9ce3e
Miguel Ferreira <miguel.ferreira@autentek.de>
2019-05-10 17:37:16 +0200

    Indonesia fix (needs further testing) Languages ordered

--- src/services/language-service.ts
+++ src/services/language-service.ts
@@ -11,7 +11,7 @@ export class LanguageService {
     private initializeListeners = [];
 
     private static STORAGE_KEY = 'LanguageService.lang';
-    private availableLanguages = ["en", "de", "it", "fr", "es", "pt", "sk", "tr", "zh", "el", "in"];
+    private availableLanguages = ["en", "de", "it", "fr", "es", "pt", "sk", "tr", "zh", "el", "id"];
 
     constructor(private translateService: TranslateService, private platform: Platform,
                 private globalization: Globalization, private storage: Storage) {
