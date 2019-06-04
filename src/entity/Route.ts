6c6ef34c1fa4b8e4a6038033b8d3942fffddd95c
Damian Scheerer <damian.scheerer@autentek.de>
2019-06-04 13:22:09 +0200

    Arrr! functionality of banners for routes works in theory

--- src/entity/Route.ts
+++ src/entity/Route.ts
@@ -281,10 +281,9 @@ export class Route {
             switch (narrative) {
                 case 'pirates':
                     return 'https://{s}.api.mapbox.com/styles/v1/tempgeocent/cj2qe6qid003a2rmrquvqgbcx/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoidGVtcGdlb2NlbnQiLCJhIjoiY2l1YTNmenEyMDAwdDJ6cWZxbG55Yjg4OSJ9.QRTz4Pi3096MtXKc_QgpWQ';
-                    //return `https://{s}.api.mapbox.com/styles/v1/tempgeocent/cj2qe6qid003a2rmrquvqgbcx/tiles/256/{z}/{x}/{y}${L.Browser.retina ? '@2x': ''}?access_token=${Helper.accessToken}`;
                 default:
                     return Helper.mapquestUrl
-                    /*http://{s}.tiles.mapbox.com/v4/${Helper.mapCode}/{z}/{x}/{y}${L.Browser.retina ? '@2x' : ''}.png?&tilesize=256&access_token=${Helper.accessToken}*/
+                
             }
         }
     }
