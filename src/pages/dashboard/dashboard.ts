a18b74173a691defb90256078807acd1ec35898e
Stanislav Ulyanenkov <stanislav.ulyanenkov@apparentmedia.de>
2019-10-16 17:41:26 +0200

    search bar styling and adding functionality, dashboard navigation moving, G O W O M B A T S

--- src/pages/dashboard/dashboard.ts
+++ src/pages/dashboard/dashboard.ts
@@ -1,25 +1,24 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the DashboardPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { IonicPage} from 'ionic-angular';
+import { NavController } from 'ionic-angular/navigation/nav-controller';
+import { NavParams } from 'ionic-angular/navigation/nav-params';
 
 @IonicPage()
 @Component({
-  selector: 'page-dashboard',
-  templateUrl: 'dashboard.html',
+    selector: 'page-dashboard',
+    templateUrl: 'dashboard.html',
 })
 export class DashboardPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    constructor(public navCtrl: NavController,
+                public navParams: NavParams) {
+    }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad DashboardPage');
-  }
+    pushSettingsPage() {
+        this.navCtrl.push('SettingsPage');
+    }
+    pushInfoPage() {
+        this.navCtrl.push('InfoPage');
+    }
 
 }
