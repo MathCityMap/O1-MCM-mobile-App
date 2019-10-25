ad9687b2c36514f4740b4bf0d086d29e402fa8ad
Miguel Ferreira <miguel.ferreira@autentek.de>
2019-10-25 15:58:24 +0200

    GPS is requested at the start of the app

--- src/pages/dashboard/dashboard.ts
+++ src/pages/dashboard/dashboard.ts
@@ -4,6 +4,8 @@ import { NavController } from 'ionic-angular/navigation/nav-controller';
 import { NavParams } from 'ionic-angular/navigation/nav-params';
 import {Helper} from "../../classes/Helper";
 import { Storage } from "@ionic/storage";
+import {GpsService} from "../../services/gps-service";
+import {timeout} from 'promise-timeout';
 
 @IonicPage()
 @Component({
@@ -18,12 +20,36 @@ export class DashboardPage {
     constructor(public navCtrl: NavController,
                 public navParams: NavParams,
                 private helper: Helper,
-                private storage: Storage) {
+                private storage: Storage,
+                private gpsService: GpsService) {
                 this.tabBarElement = document.querySelector('.tabbar');
     }
 
     async ngOnInit(){
+        /**
+         * The first time the app is opened it will ask for geolocation to prevent the map and list to show data
+         * from the default one (Frankfurt) for a few seconds
+         * The devmode variable is fetched due to not being able to do it trhough the helper service because of the delayed response.
+         */
         this.devMode = (await this.storage.get('devMode') === 'true')
+        if (!this.gpsService.getLastPosition()) {
+            // try to get position
+            try {
+                await timeout(this.gpsService.getCurrentPosition().catch(err => {
+                    console.error("Error loading GPS data", err)
+                }), 2000);
+            } catch (e) {
+                console.log("could not obtain position: " + e.message);
+                // make position check async
+                this.gpsService.getCurrentPosition().then((position) => {
+                    if (position && position.coords) {
+                    }
+                }, err => {
+                    console.error("Error loading GPS data", err)
+                });
+            }
+        }
+
     }
 
     pushSettingsPage() {
