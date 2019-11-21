2b97f6d632d36f36ab83593c9f3eda0be23a38a0
Stanislav Ulyanenkov <stanislav.ulyanenkov@apparentmedia.de>
2019-11-21 19:07:33 +0100

    progress on progress (circle)

--- src/components/route-teaser/route-teaser.ts
+++ src/components/route-teaser/route-teaser.ts
@@ -6,63 +6,61 @@ import {Helper} from "../../classes/Helper";
 
 
 @Component({
-  selector: 'route-teaser',
-  templateUrl: 'route-teaser.html',
-  //changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'route-teaser',
+    templateUrl: 'route-teaser.html',
+    //changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RouteTeaserComponent {
 
-  @Input()
-  route: Route;
+    @Input()
+    route: Route;
 
-  @Input()
-  isOnline: boolean;
+    @Input()
+    isOnline: boolean;
 
-  @Output()
-  downloadRoute = new EventEmitter<any>();
+    @Output()
+    downloadRoute = new EventEmitter<any>();
 
-  @Output()
-  removeRoute = new EventEmitter<any>();
+    @Output()
+    removeRoute = new EventEmitter<any>();
 
-  private currentProgress: number = 0;
-  private total: number = 0;
+    private currentProgress: number = 0;
+    private total: number = 0;
 
-  private completedRadius: number = 0;
+    private completedRadius: number = 339.2;
 
-  constructor( private modalsService: ModalsService,
-               private ormService: OrmService,
-               private helper: Helper) {
-  }
-
-  async ngOnInit(){
-    if(this.route && this.route.scores) {
-      let data = await this.helper.calculateProgress(this.route)
-
-      this.currentProgress = data.currentProgress;
-      this.total = data.totalTasks;
-      this.completedRadius = this.calculatePercentage();
+    constructor( private modalsService: ModalsService,
+                 private ormService: OrmService,
+                 private helper: Helper) {
     }
-  }
 
-  async doDownload(event, route: Route) {
-    event.stopPropagation();
-    if (await this.modalsService.doDownload(route)) {
-      this.downloadRoute.emit({route: route});
+    async ngOnInit(){
+        if(this.route && this.route.scores) {
+            let data = await this.helper.calculateProgress(this.route)
+
+            this.currentProgress = data.currentProgress;
+            this.total = data.totalTasks;
+            this.completedRadius = this.calculatePercentage();
+        }
     }
-  }
 
-  async deleteRoute(event, route: Route) {
-    event.stopPropagation();
-    if(await this.ormService.removeDownloadedRoute(route)){
-      this.removeRoute.emit();
+    calculatePercentage(){
+        let c = 2 * Math.PI * 54;
+        let completedPercentage = (1/this.total) *this.currentProgress;
+        return c * (1-completedPercentage);
     }
-  }
 
-  calculatePercentage(){
-    let c = 2 * Math.PI * 54;
-    let completedPercentage = (1/this.total) *this.currentProgress;
-    return c * (1-completedPercentage);
+    async doDownload(event, route: Route) {
+        event.stopPropagation();
+        if (await this.modalsService.doDownload(route)) {
+            this.downloadRoute.emit({route: route});
+        }
+    }
 
-  }
-
+    async deleteRoute(event, route: Route) {
+        event.stopPropagation();
+        if(await this.ormService.removeDownloadedRoute(route)){
+            this.removeRoute.emit();
+        }
+    }
 }
