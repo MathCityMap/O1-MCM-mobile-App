060a2bbd29a65e6ee4147b62e72525db74c9d927
Miguel Ferreira <miguel.ferreira@autentek.de>
2019-11-25 13:53:54 +0100

    add new collumns to the "prevent uglyfication" array

--- config/uglifyjs.config.js
+++ config/uglifyjs.config.js
@@ -10,7 +10,8 @@ module.exports = {
       reserved: ['InitialMigration1513274191111', 'AddImageUrlAndDownloadedFlagMigration1513679923000',
           'FailedTaskMigration1515428187000', 'AddUnlockedColumn1516037215000', 'AddCompletedColumn1519817905000',
           'HomePage', 'InfoPage', 'SettingsPage', 'ModalCmp', 'RoutesListPage', 'MapPage', 'TaskDetailPage', 'ChatPage',
-          'AddVisibleColumn1526306624000', 'AddLangCodeColumn1526306730000', 'TasksMap']
+          'AddVisibleColumn1526306624000', 'AddLangCodeColumn1526306730000', 'TasksMap',
+          'AddDownloadDateColumn15711518720000', 'AddCompletedDateColumn15713974540000']
   },
 
   /**
